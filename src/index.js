10918037e774acf91876d8009fdd8bf1a99bec56
Bvnkumar <bvnkumar007@gmail.com>
2019-07-04 12:29:03 +0530

    Added code for state and props

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import App from './App';
 import Parent from './parent'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Parent hello="narenda"/>, document.getElementById('root'));
+ReactDOM.render(<div><Parent eligible={true}/><Parent/></div>, document.getElementById('root'));
 registerServiceWorker();
--- src/parent.js
+++ src/parent.js
@@ -1,20 +1,47 @@
 import React from 'react';
 
 class Parent extends React.Component {
-    name = "naren"
+    name = "naren";
+    inerval;
     constructor(props) {
         super(props);
-        this.state = { date: new Date() }
+        this.state = { date: new Date(), eligible:true }
+        this.changeState=this.changeState.bind(this)
     }
-    componentDidMount(){
-        console.log("component did mount")
+    componentDidMount() {
+        console.log("component did mount");
+        this.interval = setInterval(() => {
+            this.tick()
+        }, 1000)
     }
-    componentWillMount(){
-        console.log("Componentwillmount")
+    componentWillMount() {
+        console.log("Componentwillmount");
+        clearInterval(this.inerval)
     }
-    render() {
-        return (<h1>Hellod {this.state.date.toLocaleTimeString()}</h1>)
+    tick() {
+        this.setState({ date: new Date() })
+    }
+    sayHello(s, e) {
+        console.log("e ", e);
+        console.log("s ", s);
+        console.log("Hello")
+    }
+   changeState(){
+       console.log("we are changing the state");
+       this.setState(state=>({eligible:!state.eligible}))
+   }
+    // render() {
+    //     return (<div><h1>Date: {this.state.date.toLocaleTimeString()}</h1><br></br><button onClick={(e) => { this.sayHello("naren", e) }}>Hello</button></div>)
+    // }
+    render(){
+        return(<div><Welcome eligible={this.state.eligible}/><button onClick={this.changeState}>Click</button></div>)
     }
 }
-
+function Welcome(props) {
+    if(props.eligible){
+        return (<p>These are eligiable </p>)
+    }else{
+        return (<p>These are not eligiable</p>)
+    }
+}
 export default Parent;
