8effbf36937a2e1498092cb6b0e10906cdd7bf88
Bvnkumar <bvnkumar007@gmail.com>
2019-07-06 00:45:14 +0530

    Added small changes

--- src/form.js
+++ src/form.js
@@ -15,7 +15,12 @@ class FormClass extends React.Component {
     render() {
         return (<div><form><label>Name:<input value={this.state.name} onChange={this.setstateValue} /></label>{this.state.name}
             <input type="checkbox" checked={this.state.checked} onChange={this.checkBox} />sdsa{this.state.checked}
-        </form> </div>)
+            <select value={'lime'} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select> </form> </div>)
     }
 }
 export default FormClass;
